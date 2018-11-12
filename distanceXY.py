4fcd2c58713025f135c889bddbf6696f90cb3712
Lucas Ward <33394997+wardl1990@users.noreply.github.com>
2018-11-12 08:41:04 -0500

    Create distanceXY.py

---
+++ distanceXY.py
@@ -0,0 +1,6 @@
+import math
+#prec. x, y are numbers
+#postc. return distance from x to y
+def distance(x,y):
+        return math.fabs(x-y)
+print "distance(-4,6) = ", distance(-4,6), "expected: 10"
