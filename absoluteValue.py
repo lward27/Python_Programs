2361f94ead9daa3f4a9cc7ded4afbd2af5ee6261
Lucas Ward <33394997+wardl1990@users.noreply.github.com>
2018-11-12 08:43:01 -0500

    Create absoluteValue.py

---
+++ absoluteValue.py
@@ -0,0 +1,6 @@
+import math
+#prec. x is number
+#post. return absolute value of x
+def absoluteValue(x):
+        return math.fabs(x)
+print "absoluteValue(-3) = ", absoluteValue(-3), "expected: 3"
