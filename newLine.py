91a3e698a4f8aa414c6cecb7642fbdb4872d2a13
Lucas Ward <33394997+wardl1990@users.noreply.github.com>
2018-11-12 08:52:35 -0500

    Create newLine.py

---
+++ newLine.py
@@ -0,0 +1,6 @@
+### the \ character toggles magic on characters.
+print "Hello there\n I am doing somthing cool."
+print "The stasi is keeping \ts on you, mr. \tRebel."
+print "\"Quotes\""
+print "A fool's directory C:\\program files\\doze"
+print "foo",  ##suppress newline
